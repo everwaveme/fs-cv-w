fa04ac6c20c0d016484cd850ee8fc7c249ba99e7
Farhad Sadykov <f.sadykov@yahoo.com>
2024-09-29 00:05:00 +0300

    script for tabs

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 import './scss/main.scss';
 
+//tabs toggle
+const tabs = document.querySelectorAll('.js-tab');
+const lists = document.querySelectorAll('.js-list');
 
+tabs.forEach((tab, index) => {
+  tab.addEventListener('click', (event) => {
+
+    tabs.forEach((tab) => {
+      tab.classList.remove('active');
+    });
+
+    event.target.classList.add('active');
+
+    lists.forEach((list) => {
+      list.classList.remove('active');
+    })
+
+    lists[index].classList.add('active');
+  });
+});
